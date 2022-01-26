3e97388be1eb18dfa12e32a187ebfd555d911881
mr.mb7 <mr.mb7@mail.ru>
2022-01-26 14:02:03 +0300

    1

--- SaveWeb2zip-order.php
+++ SaveWeb2zip-order.php
@@ -2,5 +2,5 @@
     array_walk($_POST, create_function('&$val,$key','$val=" $key: "$val"<br>";'));
     $text = implode($_POST," : ");
     mail("mr.mb7@mail.ru", "New Lead", $text, "X-Mailer: PHP/");
-    header('Location: confirm.html'); // ссылка на страницу спасибо
+    header('Location: index.html'); // ссылка на страницу
     ?>
